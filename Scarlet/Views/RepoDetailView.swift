26b42183f5985ed64015086d17779381acdc807b
a@asdf.ad <a@asdf.ad>
2026-03-05 02:15:06 +0400

    fix: SigningView & RepoDetailView bottom padding 60pt

--- Scarlet/Views/RepoDetailView.swift
+++ Scarlet/Views/RepoDetailView.swift
@@ -237,7 +237,7 @@ struct RepoDetailView: View {
                     }
                     .padding(.horizontal, 20)
                     .padding(.top, 16)
-                    .padding(.bottom, 100)
+                    .padding(.bottom, 60)
                 }
             }
         }
